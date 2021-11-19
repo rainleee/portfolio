e6bc23913fbd4d4cddfc838fa7ed4b4a65e656d9
rainlee <rainlee4401@gmail.com>
2021-11-19 15:55:03 +0900

    Feat: update my information about_me

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,12 @@ class About extends Component {
     if (this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.about;
       var hello = this.props.resumeBasicInfo.description_header;
+      var myInfo = this.props.resumeBasicInfo.description_my_info;
       var about = this.props.resumeBasicInfo.description;
+      var phoneNum = this.props.resumeBasicInfo.contact_phone;
+      var email = this.props.resumeBasicInfo.contact_email;
+      var github = this.props.resumeBasicInfo.portfolio_github;
+      var tistory = this.props.resumeBasicInfo.portfolio_tistory;
     }
 
     return (
@@ -78,12 +83,32 @@ class About extends Component {
                   >
                     <br />
                     <span className="wave">
-                      <i className="fas fa-quote-left quote"></i>
-                      {hello}
-                      <i className="fas fa-quote-right quote"></i>
+                      <div className="about-me-header">
+                        <p className="about-me-title">{hello}</p>
+                        <p className="about-me-info">{myInfo}</p>
+                      </div>
+                      <div>
+                        <ul className="contact">
+                          Contact.
+                          <li className="phone">Phone. {phoneNum}</li>
+                          <li className="email">Email. {email}</li>
+                        </ul>
+                        <ul className="contact">
+                          Channel.
+                          <li className="github">
+                            Github.
+                            <a href={github} target="_blank">{` ${github}`}</a>
+                          </li>
+                          <li className="tistory">
+                            Blog.
+                            <a
+                              href={tistory}
+                              target="_blank"
+                            >{` ${tistory}`}</a>
+                          </li>
+                        </ul>
+                      </div>
                     </span>
-                    <br />
-                    <br />
                     {about}
                   </div>
                 </div>
