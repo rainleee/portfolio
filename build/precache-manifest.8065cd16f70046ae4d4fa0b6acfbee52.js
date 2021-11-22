36af7384aaf1fc9d2a6412bd8c11d3c6bacf7d58
rainlee <rainlee4401@gmail.com>
2021-11-22 18:58:43 +0900

    Chore: delete build .gitignore file

---
+++ build/precache-manifest.8065cd16f70046ae4d4fa0b6acfbee52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc453570424e61d8f5720e7fa53fd9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86fe6c7ef9c8d051a4d1",
+    "url": "/static/css/2.9497e657.chunk.css"
+  },
+  {
+    "revision": "1667c00167303a0a6057",
+    "url": "/static/css/main.0d6416e9.chunk.css"
+  },
+  {
+    "revision": "86fe6c7ef9c8d051a4d1",
+    "url": "/static/js/2.f18e452a.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/static/js/2.f18e452a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1667c00167303a0a6057",
+    "url": "/static/js/main.3ae586c3.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
